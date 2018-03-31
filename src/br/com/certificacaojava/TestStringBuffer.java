6da53050df9a8c3e833f70914add3fec2a04d6bb
joaolago38 <joaolago35@gmail.com>
2018-03-31 11:32:13 -0300

    novos comites

--- src/br/com/certificacaojava/TestStringBuffer.java
+++ src/br/com/certificacaojava/TestStringBuffer.java
@@ -2,8 +2,8 @@ package br.com.certificacaojava;
 
 public class TestStringBuffer {
 	 public static void main(String args[]) {
-		 StringBuffer sBuffer = new StringBuffer("test");
-		 sBuffer.append(" String Buffer");
-		 System.out.println(sBuffer);  
+		 StringBuffer sBuffer = new StringBuffer();
+//		 sBuffer.append(" String Buffer");
+//		 System.out.println(sBuffer);  
 	 }
 }
---
+++ src/br/com/certificacaojava/TestaStringBuilder.java
@@ -0,0 +1,8 @@
+package br.com.certificacaojava;
+
+public class TestaStringBuilder {
+	public static void main(String[] args) {
+		StringBuilder nomes = new StringBuilder();
+		nomes.append("Carlos>").append("Maria>").append("José>").append("Renata");
+	}
+}
