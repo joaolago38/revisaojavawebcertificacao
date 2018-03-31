4604d4985852dbe30d3bfe5aaae3d3d98d673c95
joaolago38 <joaolago35@gmail.com>
2018-03-31 11:22:41 -0300

    novos comites

--- src/br/com/certificacaojava/TestStringBuffer.java
+++ src/br/com/certificacaojava/TestStringBuffer.java
@@ -1,5 +1,9 @@
 package br.com.certificacaojava;
 
 public class TestStringBuffer {
-
+	 public static void main(String args[]) {
+		 StringBuffer sBuffer = new StringBuffer("test");
+		 sBuffer.append(" String Buffer");
+		 System.out.println(sBuffer);  
+	 }
 }
